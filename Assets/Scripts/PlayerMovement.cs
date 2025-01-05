1c25fdeb5bddc2ae039c2b9a766f2587cb612966
Akhil Alex <akhilalex003z@gmail.com>
2025-01-05 22:36:36 +0530

    basic player movement setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed;
+    private float walkSpeed = 2.0f;
+    private float runSpeed = 4.0f;
+    private float jumpHeight = 10.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        playerSpeed = walkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerSpeed = runSpeed;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+
+}
